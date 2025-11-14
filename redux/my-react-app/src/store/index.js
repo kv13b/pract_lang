032d86ce4c149b8aad2c39c9453e64ad690f246e
kv13 <karthikbhatt22@gmail.com>
2025-11-14 07:48:59 +0530

    [Dev] updation to redux toolkit

--- redux/my-react-app/src/store/index.js
+++ redux/my-react-app/src/store/index.js
@@ -1,16 +1,12 @@
-import { combineReducers, createStore } from 'redux'
-
 import productsReducer from './slices/productsReducer'
 import cartReducer from './slices/CartReducer'
 import wishListReducer from './slices/wishListReducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-const reducer = combineReducers({
-  products: productsReducer,
-  cartItems: cartReducer,
-  wishList: wishListReducer,
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cartItems: cartReducer,
+    wishList: wishListReducer,
+  }
 })
-
-export const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__?.()
-)
--- redux/my-react-app/src/store/slices/CartReducer.js
+++ redux/my-react-app/src/store/slices/CartReducer.js
@@ -1,61 +1,38 @@
-import { produce } from "immer"
-// Action Types
-export const CART_ADD_ITEM = 'cart/addItem'
-const CART_REMOVE_ITEM = 'cart/removeItem'
-const CART_ITEM_INCREASE_QUANTITY = 'cart/increaseItemQuantity'
-const CART_ITEM_DECREASE_QUANTITY = 'cart/decreaseItemQuantity'
+import { createSlice } from "@reduxjs/toolkit"
 
-// Action Creators
-export function addCartItem(productData) {
-  console.log(productData, "proda")
-  return { type: CART_ADD_ITEM, payload: productData }
-}
-
-export function removeCartItem(productId) {
-  return { type: CART_REMOVE_ITEM, payload: { productId } }
-}
-
-export function decreaseCartItemQuantity(productId) {
-  return {
-    type: CART_ITEM_DECREASE_QUANTITY,
-    payload: { productId },
-  }
-}
-
-export function increaseCartItemQuantity(productId) {
-  return {
-    type: CART_ITEM_INCREASE_QUANTITY,
-    payload: { productId },
-  }
-}
-
-// Reducer
-export default function cartReducer(originalstate = [], action) {
-  return produce(originalstate, (state) => {
-    const cartItem = state.findIndex((ci) => ci.productId === action.payload.productId)
-    switch (action.type) {
-      case CART_ADD_ITEM:
-        console.log(action.payload, "this is cartitem");
-        if (cartItem !== -1) {
-          state[cartItem].quantity += 1;
-          return state;
-        }
-        state.push({ ...action.payload, quantity: 1 });
-        return state;
-      case CART_REMOVE_ITEM:
-        state.splice(cartItem, 1);
-        return state;
-      case CART_ITEM_INCREASE_QUANTITY:
+const findItemIndex = (state, action) => state.findIndex((ci) => ci.productId === action.payload.productId)
+const slice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addCartItem(state, action) {
+      const cartItem = findItemIndex(state, action);
+      if (cartItem !== -1) {
         state[cartItem].quantity += 1;
-        return state;
-      case CART_ITEM_DECREASE_QUANTITY:
-        state[cartItem].quantity -= 1;
-        if (state[cartItem].quantity === 0) {
-          state.splice(cartItem, 1);
-        }
-        return state;
-      default:
-        return state
+      } else {
+        state.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeCartItem(state, action) {
+      const cartItem = findItemIndex(state, action);
+      console.log('Removing item at index:', cartItem); // Add log for debugging
+      if (cartItem !== -1) {
+        state.splice(cartItem, 1);
+      }
+    },
+    increaseCartItemQuantity(state, action) {
+      const cartItem = findItemIndex(state, action);
+      state[cartItem].quantity += 1;
+    }, decreaseCartItemQuantity(state, action) {
+      const cartItem = findItemIndex(state, action);
+      state[cartItem].quantity -= 1;
+      if (state[cartItem].quantity === 0) {
+        state.splice(cartItem, 1);
+      }
     }
-  })
-}
+
+  }
+})
+console.log(slice, "slice");
+export const { addCartItem, removeCartItem, increaseCartItemQuantity, decreaseCartItemQuantity } = slice.actions;
+export default slice.reducer;
