3309518ed29b3fdeb7daa42e94ca4c43f715b75d
kv13 <karthikbhatt22@gmail.com>
2026-02-20 07:48:21 +0530

    uploader endpoint missing in aws

--- kbay/server/pro_service/lib/S3-bucket-stack.ts
+++ kbay/server/pro_service/lib/S3-bucket-stack.ts
@@ -1,4 +1,5 @@
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import { RemovalPolicy } from "aws-cdk-lib";
+import { BlockPublicAccess, Bucket, BucketEncryption } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 
 export class S3BucketStack extends Construct {
@@ -6,5 +7,13 @@ export class S3BucketStack extends Construct {
 
     constructor(scope: Construct, id: string) {
         super(scope, id);
+        this.bucketName = new Bucket(this, `${id}-bucket`, {
+            blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+            encryption: BucketEncryption.S3_MANAGED,
+            enforceSSL: true,
+            versioned: false,
+            removalPolicy: RemovalPolicy.DESTROY,
+        });
+
     }
 }
--- kbay/server/pro_service/lib/pro_service-stack.ts
+++ kbay/server/pro_service/lib/pro_service-stack.ts
@@ -2,15 +2,16 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { ServiceStack } from './service-stack';
 import { APiGatewayStack } from './api-gateway-stack';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { S3BucketStack } from './S3-bucket-stack';
 
 export class ProServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const { productService } = new ServiceStack(this, "ProductService", { bucket: "product-bucket" });
-    const {categoryService} = new ServiceStack(this, "CategoryService", { bucket: "category-bucket" });
-    const {dealsService} = new ServiceStack(this, "DealsService", { bucket: "deals-bucket" });
-    const {imageService} = new ServiceStack(this, "ImageService", { bucket: "image-bucket" });
+    const { bucketName } = new S3BucketStack(this, "ProductImages");
+    const { productService, categoryService, dealsService, imageService } = new ServiceStack(this, "ProductServiceStack", {
+      bucket: bucketName.bucketName
+    });
+    bucketName.grantReadWrite(imageService);
     new APiGatewayStack(this, "ApiGatewayStack", {
       productService,
       categoryService,
