a9c3b829a287ae3f997d3bd22d51f1538c657910
kv13 <karthikbhatt22@gmail.com>
2026-02-24 07:49:37 +0530

    category operations

--- kbay/server/pro_service/src/repository/category-repository.ts
+++ kbay/server/pro_service/src/repository/category-repository.ts
@@ -1,5 +1,7 @@
+import path from "path";
 import { CategoryInput } from "../dto/category-input";
 import { categories, CategoryDoc } from "../models/category-model";
+import { products } from "../models/product-model";
 
 export class CategoryRepository {
     constructor() { }
@@ -11,14 +13,41 @@ export class CategoryRepository {
             subCategory: [],
             products: []
         });
-        if(parentId){
-            const parentCategory=(await categories.findById(parentId))as CategoryDoc;
-            parentCategory.subCategories=[
+        if (parentId) {
+            const parentCategory = (await categories.findById(parentId)) as CategoryDoc;
+            parentCategory.subCategories = [
                 ...parentCategory.subCategories,
                 newCategory,
             ];
             await parentCategory.save();
         }
         return newCategory;
+    }
+    async getAllCategories(offset = 0, perpage?: number) {
+        return await categories.find({ parentId: null }).populate({
+            path: "subCategories",
+            model: "Categories",
+            populate: {
+                path: "subCategories",
+                model: "Categories"
+            }
+        }).skip(offset).limit(perpage ? perpage : 100);
+    }
+    async getCategoryById(id: string, offset = 0, perpage?: number) {
+        return categories.findById(id, {
+            products: { $slice: [offset, perpage ? perpage : 100] }
+        }).populate({
+            path: "subCategories",
+            model: "Categories",
+        });
+    }
+    async updateCategory({ id, name, displayOrder }: CategoryInput) {
+        const category = await categories.findById(id) as CategoryDoc;
+        category.name = name;
+        category.displayOrder = displayOrder;
+        return category.save();
+    }
+    async deleteCategory(id: string) {
+        return categories.deleteOne({ _id: id });
     }
 }
--- kbay/server/pro_service/src/service/category-service.ts
+++ kbay/server/pro_service/src/service/category-service.ts
@@ -13,34 +13,34 @@ export class CategoryService {
         return errorResponse(404, { message, error });
     }
     async createCategory(event: APIGatewayEvent) {
-            const input = plainToInstance(CategoryInput, JSON.parse(event.body!));
-            const error = await appValidationError(input);
-            if (error) return errorResponse(404, { message: "validation error", error });
-            const data = await this._repository.createCategory(input);
-            return successResponse(data);
-        }
-    
-        async getSingleCategory(event: APIGatewayEvent) {
-            // const data = await this._repository.getCategoryById(event.pathParameters!.id!);
-            // return successResponse(data);
-        }
-        async getCategories(event: APIGatewayEvent) {
-            // const data = await this._repository.getAllCategories();
-            // return successResponse(data);
-        }
-        async updateCategory(event: APIGatewayEvent) {
-            // const categoryId = event.pathParameters!.id!;
-            // if (!categoryId) return errorResponse(404, { message: "category id is required" });
-            // const input = plainToInstance(ProductInput, JSON.parse(event.body!));
-            // const error = await appValidationError(input);
-            // if (error) return errorResponse(404, { message: "validation error", error });
-            // input.id = categoryId;
-            // const data = await this._repository.updateCategory(input);
-            // return successResponse(data);
-        }
-        async deleteCategory(event: APIGatewayEvent) {
-            // const data = await this._repository.deleteCategory(event.pathParameters!.id!);
-            // return successResponse(data);
-        }
-    
+        const input = plainToInstance(CategoryInput, JSON.parse(event.body!));
+        const error = await appValidationError(input);
+        if (error) return errorResponse(404, { message: "validation error", error });
+        const data = await this._repository.createCategory(input);
+        return successResponse(data);
+    }
+
+    async getSingleCategory(event: APIGatewayEvent) {
+        const data = await this._repository.getCategoryById(event.pathParameters!.id!);
+        return successResponse(data);
+    }
+    async getCategories(event: APIGatewayEvent) {
+        const data = await this._repository.getAllCategories();
+        return successResponse(data);
+    }
+    async updateCategory(event: APIGatewayEvent) {
+        const categoryId = event.pathParameters!.id!;
+        if (!categoryId) return errorResponse(404, { message: "category id is required" });
+        const input = plainToInstance(CategoryInput, JSON.parse(event.body!));
+        const error = await appValidationError(input);
+        if (error) return errorResponse(404, { message: "validation error", error });
+        input.id = categoryId;
+        const data = await this._repository.updateCategory(input);
+        return successResponse(data);
+    }
+    async deleteCategory(event: APIGatewayEvent) {
+        // const data = await this._repository.deleteCategory(event.pathParameters!.id!);
+        // return successResponse(data);
+    }
+
 }
